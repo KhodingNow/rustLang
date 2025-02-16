1fa5429b7b28b5a287a5f8e28f1d5fef38e86508
Melezwa26 <phalosenzwa@gmail.com>
2025-02-16 09:48:39 +0200

    Adding variety to Programms guessing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::io;
+
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 
 fn main() {
     println!("Guess the number!");
 
-    let inani_lemfihlakalo = rand::thread_rng().gen_range(1..=100);
+  let inani_lemfihlakalo = rand::thread_rng().gen_range(1..=100);
 
     println!("Inani lemfihlakalo ngu: {inani_lemfihlakalo}");
 
@@ -13,13 +16,13 @@ fn main() {
     let mut iqashiso = String::new();
     
     
-    println!("Nceda ubhale inani oluqashileyo (qash-qash).");
+   // println!("Nceda ubhale inani oluqashileyo (qash-qash).");
 
     //let mut iqashiso = String::new();
 
     io::stdin()
-        .read_line(&mut iqashiso)
-        .expect("Failed to read line");
+          .read_line(&mut iqashiso)
+          .expect("Failed to read line");
 
     println!("Inani oliqashileyo: {}", iqashiso);
 
@@ -27,7 +30,27 @@ fn main() {
     let y = 10;
     let z = 8;
     let k = 1;
-
+    
+    
     println! ("x = {x} and y + 3 = {}", y + 3);
-    println! ("z = {z} and k + 2 = {}", k - 10)
+    println! ("z = {z} and k + 2 = {}", k - 10);
+
+    let mut iqashiso = String::new();
+
+    io::stdin()
+        .read_line(&mut iqashiso)
+        .expect("Failed to read line");
+
+    let iqashiso: u32 = iqashiso.trim().parse().expect("Nceda ubhale inani");
+
+    println!("Uqashe: {iqashiso}");
+
+
+    println! ("Uqashisele: {iqashiso}");
+
+    match iqashiso.cmp(&inani_lemfihlakalo) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 }
